170993e45b776a6879cc73df890d46e1bd27542d
js.min <mjs920415@gmail.com>
2024-01-18 22:26:38 +0900

    JPQL 관련 예시 추가

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,28 +1,34 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // 웹서버당 1개씩만 생성
 
-        EntityManager em = emf.createEntityManager();
+        EntityManager em = emf.createEntityManager(); // db 커낵션과 유사한 개념
 
         EntityTransaction tx = em.getTransaction();
 
-        tx.begin();
+        tx.begin();  // transaction 시작, JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
         try{
-            Member member = em.find(Member.class,1);
-            System.out.println("member.getId() = " + member.getId());
-            System.out.println("member.getName() = " + member.getName());
 
-            member.setName("HelloJPA");
+        //    Member member = em.find(Member.class,1); // DB에서 조회
 
-            tx.commit();
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();  // JPQL:  Entity 객체를 대상으로 sql문이 실행된다.
+
+            for (Member member1 : result) {
+                System.out.println("member1.getName() = " + member1.getName());
+            }
+       //     System.out.println("member.getId() = " + member.getId());
+       //     System.out.println("member.getName() = " + member.getName());
+
+       //     member.setName("HiJPA"); // 마치 자바컬랙션 다루듯이 set, transaction이 커밋되는 시점에 JPA가 체크하여 DB update를 수행
+
+            tx.commit(); //transaction 커밋
+
         } catch (Exception e){
             tx.rollback();
         } finally {
