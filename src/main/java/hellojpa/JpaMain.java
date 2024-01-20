cc154b204328ef7de2fc99f6d6b3d1902a7a644b
js.min <mjs920415@gmail.com>
2024-01-20 10:30:36 +0900

    flush 테스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,10 +17,11 @@ public class JpaMain {
 
             //    Member member = em.find(Member.class,1); // DB에서 조회
 
+            int testInt = 12345;
             // 비영속
             Member member = new Member();
-            member.setId(1000);
-            member.setName("HelloJPA");
+            member.setId(testInt);
+            member.setName("HelloJPA999");
 
 
             System.out.println("=== BEFORE === ");
@@ -28,10 +29,15 @@ public class JpaMain {
             em.persist(member); // DB저장이 아닌, 1차 캐쉬에 저장, DB저장은 tx.commit 될때 !
             System.out.println("=== AFTER === ");
 
-            Member member1 = em.find(Member.class, 1000); // select 쿼리가 안나간다. Why? DB조회가 아닌, 영속성컨텍스트 1차캐시에 이미 존재하기 때문에, find 해서 return
-            Member member2 = em.find(Member.class, 1000);
+            Member member1 = em.find(Member.class, testInt); // select 쿼리가 안나간다. Why? DB조회가 아닌, 영속성컨텍스트 1차캐시에 이미 존재하기 때문에, find 해서 return
+            Member member2 = em.find(Member.class, testInt);
+
+            System.out.println("flush 시작 ! "); // 변경탐지(스냅샷 vs 현재값 비교) -> Update 쿼리 SQL 저장소에 저장 -> DB SQL문 전송(Update뿐만 아니라 모든 SQL저장소에 있는 SQL문 전송!)
+            em.flush();
+            System.out.println("flush 끝 ! ");
 
             System.out.println("result == " + (member1 == member2) ); // 영속성 컨택스트의 동일성 보장, 둘다 1차캐시에서 find
+
 
             // 준영속
             //  em.detach(member);
